5ebbb5b68f64a8e060fd42417a8ea9964b4a399d
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-08 13:30:36 -0300

    0703 Definindo a Interface IPersonService

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
