88392e5fe76617241032c31db7005867f6aae55d
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-11 09:13:33 -0300

    Removendo exemplo de serialização Json

--- RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,19 +1,14 @@
-using System.Text.Json.Serialization;
+// using System.Text.Json.Serialization;
 
 namespace RestWithASPNETUdemy.Data.VO
 {
     public class PersonVO
     
     {
-        [JsonPropertyName("code")]
         public long Id { get; set; }
-        [JsonPropertyName("first_name")]
         public string FirstName { get; set; }
-        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
-        [JsonIgnore]
         public string Address { get; set; }
-        [JsonPropertyName("sex")]
         public string Gender { get; set; }
 
     }
