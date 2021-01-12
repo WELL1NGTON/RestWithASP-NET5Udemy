ce504e1ce18512d1ff19afa914c0d461b4aec865
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-12 16:43:10 -0300

    2709 O Conceito de Propriedade e Estado

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Header from './Header';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Header title="Client REST Udemy">Client REST Udemy</Header>;
 }
---
+++ client/src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default function Header(props) {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      <h1>{props.children}</h1>
+    </header>
+  );
+}
