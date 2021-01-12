04a4e68598271598bcac99cfaa5d53b4e9b92d9e
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-12 17:42:01 -0300

    2714 Configurando as Rotas com React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Login from './pages/Login';
+import Routes from './routes';
 import './global.css';
 
 export default function App() {
-  return <Login />;
+  return <Routes />;
 }
