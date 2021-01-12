d3c0188eadce683ef6cd10a76f846797ff88b510
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-12 17:12:16 -0300

    2712 Definindo a Estrutura HTML da Tela de Login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './styles.css';
+import logoImage from '../../assets/logo.svg';
+import padlock from '../../assets/padlock.png'
 
-export default function Login(props) {
+export default function Login() {
   return (
-    <header>
-      <h1>Welcome</h1>
-    </header>
+    <div className="login-container">
+        <section className="form">
+            <img src={logoImage} alt="Erudio Logo"/>
+            <from>
+                <h1>Access your Account</h1>
+                <input placeholder="Username"/>
+                <input type="password" placeholder="Password"/>
+                
+                <button type="submit">Login</button>
+            </from>
+        </section>
+        <img src={padlock} alt="Login"/>
+    </div>
   );
 }
