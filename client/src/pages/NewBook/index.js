600596a26ed631d42ebfa1876e94e4c72c7d20f9
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-13 10:53:29 -0300

    2727 Carregando os Dados do Livro a ser Editado

--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,13 +1,14 @@
 import './styles.css';
 
 import { Link, useHistory, useParams } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { FiArrowLeft } from 'react-icons/fi';
 import api from '../../services/api';
 import logoImage from '../../assets/logo.svg';
 
 export default function NewBook() {
+  const [id, setId] = useState(null);
   const [author, setAuthor] = useState('');
   const [title, setTitle] = useState('');
   const [launchDate, setLaunchDate] = useState('');
@@ -15,6 +16,36 @@ export default function NewBook() {
   const { bookId } = useParams();
 
   const history = useHistory();
+
+  const accessToken = localStorage.getItem('accessToken');
+
+  const authorization = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  useEffect(() => {
+    if (bookId == '0') return;
+    else loadBook();
+  }, [bookId]);
+
+  async function loadBook() {
+    try {
+      const response = await api.get(`api/Book/v1/${bookId}`, authorization);
+
+      const adjustedDate = response.data.launchDate.split('T', 10)[0];
+
+      setId(response.data.id);
+      setTitle(response.data.title);
+      setAuthor(response.data.author);
+      setPrice(response.data.price);
+      setLaunchDate(adjustedDate);
+    } catch (error) {
+      alert('Error recovering Book! Try again!');
+      history.push('/books');
+    }
+  }
 
   async function createNewBook(e) {
     e.preventDefault();
