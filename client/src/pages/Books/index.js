4dfae785a2e93dd427a795ce0ee05964d4c0cc72
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-13 10:29:53 -0300

    2724 Integrando a Tela de Listagem de Livros à API

--- client/src/pages/Books/index.js
+++ client/src/pages/Books/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
-
 import './styles.css';
+
+import { FiEdit, FiPower, FiTrash2 } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
 import logoImage from '../../assets/logo.svg';
 
 export default function Books() {
+  const [books, setBooks] = useState([]);
+
+  const userName = localStorage.getItem('userName');
+
+  const accessToken = localStorage.getItem('accessToken');
+
+  const history = useHistory();
+
+  useEffect(() => {
+    api
+      .get('api/Book/v1/asc/5/1', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => setBooks(response.data.list));
+  }, [accessToken]);
+
   return (
     <div className="book-container">
       <header>
         <img src={logoImage} alt="Erudio" />
         <span>
-          Welcome, <strong>Leandro</strong>!
+          Welcome, <strong>{userName.toLowerCase()}</strong>!
         </span>
         <Link className="button" to="books/new">
           Add New Book
@@ -22,113 +42,32 @@ export default function Books() {
       </header>
       <h1>Registered Books</h1>
       <ul>
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Pulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2017</p>
+        {books.map((book) => (
+          <li key={book.id}>
+            <strong>Title:</strong>
+            <p>{book.title}</p>
+            <strong>Author:</strong>
+            <p>{book.author}</p>
+            <strong>Price:</strong>
+            <p>
+              {Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(book.price)}
+            </p>
+            <strong>Release Date:</strong>
+            <p>
+              {Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}
+            </p>
 
-          <button type="button">
-            <FiEdit size={20} color="#251fc5" />
-          </button>
-          <button type="button">
-            <FiTrash2 size={20} color="#251fc5" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Pulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2017</p>
-
-          <button type="button">
-            <FiEdit size={20} color="#251fc5" />
-          </button>
-          <button type="button">
-            <FiTrash2 size={20} color="#251fc5" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Pulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2017</p>
-
-          <button type="button">
-            <FiEdit size={20} color="#251fc5" />
-          </button>
-          <button type="button">
-            <FiTrash2 size={20} color="#251fc5" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Pulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2017</p>
-
-          <button type="button">
-            <FiEdit size={20} color="#251fc5" />
-          </button>
-          <button type="button">
-            <FiTrash2 size={20} color="#251fc5" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Pulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2017</p>
-
-          <button type="button">
-            <FiEdit size={20} color="#251fc5" />
-          </button>
-          <button type="button">
-            <FiTrash2 size={20} color="#251fc5" />
-          </button>
-        </li>
-
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Pulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2017</p>
-
-          <button type="button">
-            <FiEdit size={20} color="#251fc5" />
-          </button>
-          <button type="button">
-            <FiTrash2 size={20} color="#251fc5" />
-          </button>
-        </li>
+            <button type="button">
+              <FiEdit size={20} color="#251fc5" />
+            </button>
+            <button type="button">
+              <FiTrash2 size={20} color="#251fc5" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
